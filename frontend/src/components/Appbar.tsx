b12af8aad11c3d18f54e6a570338520bf10d61ed
Vaibhav Pal <vaibhavpal9935@gmail.com>
2024-09-21 18:43:42 +0530

    changes made

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,24 +1,59 @@
-
-import { BarAvatar } from "./BarAvatar"
-import { Link } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { BarAvatar } from "./BarAvatar";
+import { Link, useLocation } from "react-router-dom";
 
 export const Appbar = () => {
-     const name = localStorage.getItem("name") || "Anonymous";
-   
-    return <div className="border-b flex justify-between px-10 py-4">
-         <Link to={'/blogs'} className="flex flex-col justify-center font-semibold cursor-pointer text-3xl font-serif">
-            Medium
-        </Link> 
-        <div>
-            <Link to={'/publish'}>
-                <button type="button" className="mr-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Create a Blog</button>
-            </Link>
-            {/* <Avatar size={"big"} name="Vaibhav"></Avatar> */}
-           
-            <BarAvatar name={name}></BarAvatar>
-        </div>
+  const name = localStorage.getItem("name") || "Anonymous";
+  const [click, setClick] = useState(true);
+  const location = useLocation();  // Get the current route
+
+  const touch = () => {
+    setClick(!click); 
+  };
+
+
+  useEffect(() => {
+    if (location.pathname === '/publish') {
+      setClick(false); 
+    }
+  }, [location]);
+
+  return (
+    <div className="border-b flex justify-between px-10 py-5">
+      <Link
+        to={'/blogs'}
+        className="flex flex-col justify-center font-semibold cursor-pointer text-3xl font-serif"
+      >
+        Medium
+      </Link>
+
+      <div className="flex">
+        {click && (
+          <Link to={'/publish'} onClick={touch}>
+            <button
+              type="button"
+              className="flex mr-4 focus:outline-none hover:text-black text-slate-500 rounded-lg text-md px-5 py-2.5 me-2 mb-2 font-semibold"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6 mr-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </svg>
+              <div className="mt-0.5">Write</div>
+            </button>
+          </Link>
+        )}
+        <BarAvatar name={name} />
+      </div>
     </div>
-
-}
-
-
+  );
+};
--- frontend/src/components/BarAvatar.tsx
+++ frontend/src/components/BarAvatar.tsx
@@ -49,11 +49,21 @@ export const BarAvatar = ({ name }: { name: string }) => {
           className="absolute right-0 mt-2 w-44 divide-y divide-gray-100 rounded-md shadow  bg-white"
         >
           <ul className="py-2 text-sm text-gray-700 dark:text-gray-900 font-semibold">
-            <li className="block px-4 py-2 cursor-pointer">
-              <button onClick={Profile}>Profile</button>
+            <li className="block px-4 py-2 cursor-pointer ">
+              <button onClick={Profile} className="flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mr-2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+                Profile
+              </button>
             </li>
             <li className="block px-4 py-2 cursor-pointer">
-              <button onClick={logout}>Sign out</button>
+                <button onClick={logout} className="flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 mr-2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9" />
+                </svg>
+                Sign out
+              </button>
             </li>
           </ul>
         </div>
